0adc3c0e97a1e1906201afb08f9d4111820efd0e
twig <twigwhite@gmail.com>
2016-01-30 14:43:02 -0600

    Game mode now uses the PlayerController (which shows mouse cursor).

---
+++ Source/ggj16_slamjam/ggj16_slamjamPlayerController.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ggj16_slamjam.h"
+#include "ggj16_slamjamPlayerController.h"
+
+Aggj16_slamjamPlayerController::Aggj16_slamjamPlayerController()
+{
+	bShowMouseCursor = true;
+}
---
+++ Source/ggj16_slamjam/ggj16_slamjamPlayerController.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "ggj16_slamjamPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GGJ16_SLAMJAM_API Aggj16_slamjamPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	Aggj16_slamjamPlayerController();
+};
